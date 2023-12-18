5fa8bc0f7590e242fbe8377c19d10411863d927e
Mariana P Braga <mpiresbr@gmail.com>
2023-12-18 15:03:51 +0100

    Add coin flipping vignette

--- R/run_treeppl.R
+++ R/run_treeppl.R
@@ -1,12 +1,28 @@
-run_treeppl <- function(source = NULL, method = "smc-bpf", data = NULL, samples = 1000) { # smc-apf
+#' Compile and run a TreePPL program
+#'
+#' @param dir The directory that contains the TreePPL code and input file. The executable and the output files will be written here.
+#' @param source Name of file with TreePPL program.
+#' @param method Inference method to be used.
+#' @param data Name of JSON file with input data.
+#' @param samples The number of samples during inference.
+#'
+#' @return A data frame with sampled values, log weights, normalized weights, and the normalizing constant for all samples.
+#' @export
+#'
+#' @examples
+#' output <- run_treeppl(dir = system.file("extdata", package = "treepplr"), source = "coin.tppl", data = "coin.json", samples = 10)
+
+run_treeppl <- function(dir = NULL, source = NULL, method = "smc-bpf", data = NULL, samples = 1000) { # smc-apf
 
   # Compile program
-  system2(command = "tpplc", args = c(source, paste0("-m ", method)))
+  system2(command = "tpplc", args = c(paste0(dir,"/", source),
+                                      paste0("-m ", method)))
   # which arguments are necessary other than method?
   # should the executable go to a temporary folder and be delete afterwards?
 
   # run
-  system2(command = "./out", args = c(data, paste0(samples, " 1")), stdout = "stdout.json")
+  system2(command = "./out", args = c(paste0(dir,"/", data),
+                                      paste0(samples, " 1")), stdout = "stdout.json")
 
   # read output
   output <- jsonlite::fromJSON("stdout.json") %>%
@@ -16,8 +32,9 @@ run_treeppl <- function(source = NULL, method = "smc-bpf", data = NULL, samples 
   return(output)
 }
 
-norm_weights <- function(weights_vector) { #input log weights from TreePPL
-  lweights <- as.numeric(weights_vector) #making sure weights are numeric vector
-  weights <- exp(lweights - max(lweights)) #exponentiate the difference between the weight and the maximum of the weights
-  weights / sum(weights) #Normalising weights
+# Function to get normalize weights from log weights
+norm_weights <- function(weights_vector) { # input log weights from TreePPL
+  lweights <- as.numeric(weights_vector) # making sure weights are numeric vector
+  weights <- exp(lweights - max(lweights)) # exponentiate the difference between the weight and the maximum of the weights
+  weights / sum(weights) # normalising weights
 }
---
+++ R/treepplr_example.R
@@ -0,0 +1,18 @@
+#' TreePPL example files
+#'
+#' @param path Optional. Name of example file. If NULL, returns a list of all example files.
+#'  in the `extdata` directory.
+#'
+#' @return A list of files or a path to one example file.
+#' @export
+#'
+#' @examples
+#' treepplr_example()
+#' treepplr_example("coin.json")
+treepplr_example <- function(path = NULL) {
+  if (is.null(path)) {
+    dir(system.file("extdata", package = "treepplr"))
+  } else {
+    system.file("extdata", path, package = "treepplr", mustWork = TRUE)
+  }
+}
