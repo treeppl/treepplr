4566a990985c4b4e9de5c9b94affd4534fed152d
Mariana P Braga <mpiresbr@gmail.com>
2024-01-09 09:31:44 +0100

    Update function run_treeppl

---
+++ R/treeppl_input.R
@@ -0,0 +1,23 @@
+#' Prepare input for inference with TreePPL
+#'
+#' This function takes R objects and writes out an JSON file to be used by `run_treeppl()`.
+#'
+#' @param object_list A list of R objects to be included in the JSON input file.
+#' @param file_path The complete path to where the JSON file is to be written.
+#'
+#' @return Path to a JSON file
+#' @export
+#'
+#' @examples
+treeppl_input <- function(object_list, file_path){
+
+  # process one element at a time?
+
+  # figure out the class and if anything special is necessary
+
+  # write json with input data
+  input_json <- jsonlite::toJSON(data, dataframe = "columns")
+  write(input_json, paste0(dir,"/input.json"))
+
+
+}
